6aa8978c89786b9e410a745e5a30699c95ff26bb
Khalid Bello <khalidbello279@gmail.com>
2020-12-17 10:48:01 +0100

    alert message

--- FishVille/main.js
+++ FishVille/main.js
@@ -1,3 +1,23 @@
- $('[data-toggle="popover"]').popover(); 
+// $('[data-toggle="popover"]').popover(); 
+//
+//$('#myPopover').popover();
 
-$('#myPopover').popover();
+
+document.getElementById("order-button").addEventListener("click", function(){
+    
+    if($("input[type=text]").val() !== "") {
+        
+        alert("your order has been received and is now being processed.");
+    }
+})
+
+document.getElementById("inputBox").addEventListener("click", function(){
+    
+    if($(".input").val() !== "") {
+        
+        alert("Your message has been received. We will be in touch soon");
+    }
+})
+
+
+
