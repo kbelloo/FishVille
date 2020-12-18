36ba35db2a7da01497633824d2c1121c0f90205f
Khalid Bello <khalidbello279@gmail.com>
2020-12-18 16:35:38 +0100

    order section modal

--- FishVille/main.js
+++ FishVille/main.js
@@ -1,15 +1,31 @@
-// $('[data-toggle="popover"]').popover(); 
-//
-//$('#myPopover').popover();
 
 
-document.getElementById("order-button").addEventListener("click", function(){
-    
+let overlay = document.getElementById("overlay");
+let openModal = document.getElementById('open-modal');
+let closeModal = document.getElementById('close-modal');
+let orderButton = document.getElementById('order-button');
+
+
+overlay.style.display = "none";
+
+openModal.addEventListener("click", function(e){
+   
     if($("input[type=text]").val() !== "") {
         
-        alert("your order has been received and is now being processed.");
-    }
+        overlay.style.display = "block";
+        e.preventDefault();
+        orderButton.style.display = "none";
+        
+    }  
+});
+
+closeModal.addEventListener("click", function(e){
+    
+    overlay.style.display = "none";
+    e.preventDefault();
+    orderButton.style.display = "block";
 })
+
 
 document.getElementById("inputBox").addEventListener("click", function(){
     
